903d0ca8c233bd66758d6a3f733bd9650f487c29
daf672 <dieganfer27@gmail.com>
2026-02-24 02:26:04 +0100

    Creación de la clase Converters.kt que se encarga de convertir la ID de las entidades para su correcto uso en Room

---
+++ app/src/main/java/com/dieang/energym/data/local/Converters.kt
@@ -0,0 +1,12 @@
+package com.dieang.energym.data.local
+
+import androidx.room.TypeConverter
+import java.util.UUID
+
+class Converters {
+    @TypeConverter
+    fun fromUUID(uuid: UUID?): String? = uuid?.toString()
+
+    @TypeConverter
+    fun toUUID(uuid: String?): UUID? = uuid?.let { UUID.fromString(it) }
+}
