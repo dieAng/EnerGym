d8f3a5421c4ff46b4c801dc78f451f44487f729c
diego <dieganfer.dev@gmail.com>
2026-04-12 22:17:56 +0200

    Creación de Routes.kt (todas las rutas centralizadas)

--- app/src/main/java/com/dieang/energym/ui/navigation/Route.kt
+++
@@ -1,9 +0,0 @@
-package com.dieang.energym.ui.navigation
-
-sealed class Route(val route: String) {
-    object Login : Route("login")
-    object Home : Route("home")
-    object Perfil : Route("perfil")
-    object Rutinas : Route("rutinas")
-    object Recetas : Route("recetas")
-}
---
+++ app/src/main/java/com/dieang/energym/ui/navigation/Routes.kt
@@ -0,0 +1,35 @@
+package com.dieang.energym.ui.navigation
+
+object Routes {
+
+    // AUTH
+    const val LOGIN = "login"
+
+    // HOME
+    const val HOME = "home"
+
+    // USUARIO
+    const val USUARIOS = "usuarios"
+    const val USUARIO_DETAIL = "usuario/{id}"
+
+    // RECETAS
+    const val RECETAS = "recetas"
+    const val RECETA_DETAIL = "receta/{id}"
+
+    // RUTINAS
+    const val RUTINAS = "rutinas"
+    const val RUTINA_DETAIL = "rutina/{id}"
+
+    // SESIONES
+    const val SESIONES = "sesiones"
+    const val SESION_DETAIL = "sesion/{id}"
+
+    // POSTS
+    const val POSTS = "posts"
+    const val POST_DETAIL = "post/{id}"
+
+    // MENSAJES
+    const val MENSAJES = "mensajes"
+    const val CHAT = "chat/{u1}/{u2}"
+}
+
