605700c74d199a88589d6ee4dbe26117cada4162
diego <dieganfer.dev@gmail.com>
2026-04-12 22:47:46 +0200

    Correción de DataStoreModule.kt

---
+++ app/src/main/java/com/dieang/energym/di/DataStoreModule.kt
@@ -0,0 +1,40 @@
+package com.dieang.energym.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStoreFile
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import com.dieang.energym.data.local.datastore.TokenProvider
+import com.dieang.energym.data.local.datastore.UserDataStore
+import com.dieang.energym.data.local.datastore.UserStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create {
+            context.dataStoreFile("app_prefs.preferences_pb")
+        }
+
+    @Provides @Singleton
+    fun provideTokenProvider(
+        dataStore: DataStore<Preferences>
+    ): TokenProvider = TokenDataStore(dataStore)
+
+    @Provides @Singleton
+    fun provideUserStore(
+        dataStore: DataStore<Preferences>
+    ): UserStore = UserDataStore(dataStore)
+}
