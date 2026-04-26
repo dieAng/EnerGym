68f256bbf1cd21b8a0193d187d5c69483bab53db
diego <dieganfer.dev@gmail.com>
2026-04-27 00:18:57 +0200

    Implementación Ejercicio

--- app/src/main/java/com/dieang/energym/data/mappers/EjercicioMapper.kt
+++ app/src/main/java/com/dieang/energym/data/mappers/EjercicioMapper.kt
@@ -2,6 +2,29 @@ package com.dieang.energym.data.mappers
 
 import com.dieang.energym.data.local.entity.EjercicioEntity
 import com.dieang.energym.data.remote.dto.response.EjercicioResponseDto
+import com.dieang.energym.domain.model.Ejercicio
+
+// Entity → Domain
+fun EjercicioEntity.toDomain() = Ejercicio(
+    id = id,
+    nombre = nombre,
+    grupoMuscular = grupoMuscular,
+    equipo = equipo,
+    descripcion = descripcion,
+    imagenUrl = imagenUrl,
+    videoUrl = videoUrl
+)
+
+// Domain → Entity
+fun Ejercicio.toEntity() = EjercicioEntity(
+    id = id,
+    nombre = nombre,
+    grupoMuscular = grupoMuscular,
+    equipo = equipo,
+    descripcion = descripcion,
+    imagenUrl = imagenUrl,
+    videoUrl = videoUrl
+)
 
 // Response → Entity
 fun EjercicioResponseDto.toEntity() = EjercicioEntity(
