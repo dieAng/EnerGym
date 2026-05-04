3f09f3740709362cb37595630bf531860b680089
DAM204 <dam01.2025@gmail.com>
2026-05-04 09:39:16 +0200

    Correciones

--- app/src/main/java/com/dieang/energym/domain/repository/PostRepository.kt
+++ app/src/main/java/com/dieang/energym/domain/repository/PostRepository.kt
@@ -1,9 +1,7 @@
 package com.dieang.energym.domain.repository
 
-import com.dieang.energym.data.remote.dto.request.ComentarioCreateRequestDto
 import com.dieang.energym.data.remote.dto.request.PostCreateRequestDto
 import com.dieang.energym.domain.model.Post
-import java.util.UUID
 
 interface PostRepository {
 
@@ -12,8 +10,4 @@ interface PostRepository {
     suspend fun getPosts(): List<Post>
 
     suspend fun createPost(request: PostCreateRequestDto): Post
-
-    suspend fun likePost(postId: UUID, usuarioId: UUID)
-
-    suspend fun addComentario(postId: UUID, request: ComentarioCreateRequestDto, usuarioId: UUID)
 }
